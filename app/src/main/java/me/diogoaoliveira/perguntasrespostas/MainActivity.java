57ec4cfa6173783856a85e062bf448cf74b7d1b5
Diogo A <diogo.augustoo@gmail.com>
2016-11-25 10:27:04 -0200

    Added Questionario and Perguntas Activities

--- app/src/main/java/me/diogoaoliveira/perguntasrespostas/MainActivity.java
+++ app/src/main/java/me/diogoaoliveira/perguntasrespostas/MainActivity.java
@@ -1,7 +1,10 @@
 package me.diogoaoliveira.perguntasrespostas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button perguntas = (Button)findViewById(R.id.cadastroPerguntas);
+        Button questionario = (Button)findViewById(R.id.reponderQuestionario);
+
+        perguntas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                entrarCadastroPerguntas();
+            }
+        });
+
+        questionario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                responderQuestionario();
+            }
+        });
     }
+
+    public void entrarCadastroPerguntas() {
+        Intent intent = new Intent(this, PerguntasActivity.class);
+        startActivity(intent);
+    }
+
+    public void responderQuestionario() {
+        Intent intent = new Intent(this, QuestionarioActivity.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/me/diogoaoliveira/perguntasrespostas/PerguntasActivity.java
@@ -0,0 +1,13 @@
+package me.diogoaoliveira.perguntasrespostas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PerguntasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perguntas);
+    }
+}
